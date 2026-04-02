f5b9a426d789413023c4bc391bac369e55bc62e7
fozilovyosin2010 <umnovanika1@gmail.com>
2026-04-03 00:18:01 +0500

    adding card

--- src/App.jsx
+++ src/App.jsx
@@ -11,72 +11,121 @@ import image6 from "./imgs/image6.png";
 import image7 from "./imgs/image7.png";
 
 import Sh2 from "./imgs/Sh2.png";
+import dots from "./imgs/Dots1.png";
+import menuIcon from "./imgs/menuIcon.png";
+
+import energy from "./imgs/energy.png";
+import like from "./imgs/like.png";
+import sales from "./imgs/sales.png";
+import call from "./imgs/call.png";
+
+import Card from "./Component/Card";
 
 class App extends Component {
   render() {
+    const cardList = [
+      {
+        icon: call,
+        title: "Support",
+        des: "Delivering faster and more personalized support with shared screens and cool design systems for Figma",
+        bgColor: "#E84393",
+      },
+      {
+        icon: sales,
+        title: "Sales growth",
+        des: "Identify qualified customers with easy-to-use live chat messaging and AI-based Sales Bot",
+        bgColor: "#0984E3",
+      },
+      {
+        icon: energy,
+        title: "Coponents-driven",
+        des: "Delivering faster and more personalized support with shared screens and cool design systems for Figma",
+        bgColor: "#6C5CE7",
+      },
+      {
+        icon: like,
+        title: "Swap the icon",
+        des: "You can toggle to any icon within Instances and customize outlined stroke to more bolder or lighter",
+        bgColor: "#00B894",
+      },
+    ];
     const linkList = ["Who we are", "What we do", "Contact us"];
     return (
-      <div className="header border-b border-b-[#FFFFFF] shadow-[0_4px_8px_#26323814]">
-        <div className="px-[40px] max-w-[1440px] m-[0_auto] ">
-          <div className="px-[20px] flex items-center justify-between">
-            <a href="#" className="flex items-center gap-3">
-              <img src={logo} alt="" />
-              <div className="text-[30px] font-[700]">Grid</div>
-            </a>
-            <nav>
-              <div className="flex gap-6">
-                <a href="#" className="font-[700] mr-[15px]">
-                  How it works
-                </a>
-                <ul className="list-disc marker:text-[#FDCB6E] flex gap-6">
-                  {linkList.map((e, i) => {
-                    return (
-                      <li key={i}>
-                        <a href="#" className="text-purple-500">
-                          {e}
-                        </a>
-                      </li>
-                    );
-                  })}
-                </ul>
-              </div>
+      <div className="inter">
+        <div className="header border-b border-b-[#ccc] shadow-[0_4px_8px_#26323814]">
+          <div className="px-[40px] sec p-[10px_20px] flex items-center justify-between max-md:p-[5px_10px]">
+            <div className="flex items-center gap-1">
+              <button className="w-[40px] h-[40px] hidden max-md:flex gap-1 flex-col">
+                <img src={menuIcon} alt="" />
+              </button>
+              <a href="#" className="flex items-center gap-1">
+                <img src={logo} alt="" className="max-md:hidden" />
+                <div className="text-[30px] font-[700] text-[#5C5C5C] max-md:text-[24px]">
+                  Grid
+                </div>
+              </a>
+            </div>
+            <nav className="flex gap-6 max-md:hidden">
+              <a href="#" className="font-[700] mr-[15px]">
+                How it works
+              </a>
+              <ul className="list-disc marker:text-[#FDCB6E] flex gap-6">
+                {linkList.map((e, i) => {
+                  return (
+                    <li key={i}>
+                      <a href="#" className="text-purple-500">
+                        {e}
+                      </a>
+                    </li>
+                  );
+                })}
+              </ul>
             </nav>
-            <button className="bg-[#6C5CE7] text-[#fff] p-[8px_16px] rounded-[100px]">
+            <button className="bg-[#6C5CE7] text-[#fff] p-[8px_32px] rounded-[100px] max-md:rounded-[8px] max-md:p-[4px_24px]">
               Sign
             </button>
           </div>
         </div>
         <div
-          className="container1 max-w-full "
+          className="container1 min-w-[300px] overflow-hidden"
           style={{
-            backgroundImage: `url(${Sh2})`,
+            backgroundImage: `url(${Sh2}) `,
             backgroundRepeat: "no-repeat",
-            backgroundSize: "100%",
+            backgroundSize: "100% 50%",
             backgroundPosition: "top-right",
           }}
         >
-          <div className="flex flex-col items-center">
-            <div className="block1 font-[700] py-4 flex flex-col items-center">
-              <div className="text-[96px]">
-                Combine <span className="text-red-600">fine</span> images
+          <div
+            className="flex flex-col items-center"
+            style={{
+              backgroundImage: `url(${dots})`,
+              backgroundRepeat: "no-repeat",
+              backgroundPositionY: "top",
+              backgroundPositionX: "right",
+              backgroundSize: "150px 150px",
+            }}
+          >
+            <div className="block1 font-[700] py-6 flex flex-col items-center">
+              <div className="text-[96px] text-center max-md:text-[64px]">
+                Combine
+                <span className="text-red-600"> fine</span> images
               </div>
-              <div className="text-[64px]">To represent a product</div>
+              <div className="text-[64px] max-md:text-[24px] text-[#2D3436]">
+                To represent a product
+              </div>
             </div>
-            <div className="block2 flex items-start gap-5">
-              <div className="miniBlock1">
-                <div className="max-w-[270px] flex flex-col gap-5 ">
-                  <div className="max-w-full">
-                    Use mixed grid with imagery, replace with your own photos
-                    and descriptions
-                  </div>
-                  <div>
-                    <img src={image2} alt="" className="max-w-full" />
-                  </div>
-                  <button className="bg-[#6C5CE7] w-full text-[#fff] p-[8px_16px] rounded-[100px]">
-                    Sign
-                  </button>
+            <div className="block2 flex items-start gap-5 px-6">
+              <div className="miniBlock1 max-w-[270px] flex flex-col justify-between gap-5 ">
+                <div className="max-w-full">
+                  Use mixed grid with imagery, replace with your own photos and
+                  descriptions
                 </div>
-                <div></div>
+                <div>
+                  <img src={image2} alt="" className="max-w-full" />
+                </div>
+                <button className="bg-[#6C5CE7] w-full text-[#fff] p-[8px_16px] rounded-[100px]">
+                  Sign
+                </button>
               </div>
 
               <div className="miniBlock2 flex flex-col gap-5">
@@ -104,18 +153,31 @@ class App extends Component {
             </div>
           </div>
         </div>
-        <div className="container2 max-w-[1440px] m-[0_auto]">
+        <div className="container2 sec p-[20px]">
           <div className="block1">
-            <div className="font-[700] text-[32px]">
+            <div className="font-[700] text-[32px] text-[#2D3436] max-md:text-[24px]">
               Delivering good designs since 1954 🚚💨
             </div>
-            <div className="max-w-[1170px] font-[400] text-[24px]">
+            <div className="max-w-[1170px] font-[400] text-[24px] max-md:text-[18px]">
               We’re the first multi-purpose design kit solutions for businesses.
               We help you bridge gaps between your layouts, templates and
               developers to empower all involved.
             </div>
           </div>
-          <div className="block2"></div>
+          <div className="block2 grid grid-cols-4 max-md:grid-cols-2 gap-[30px] py-3 max-lg:">
+            {cardList.map((e, i) => {
+              return <Card key={i} {...e} />;
+            })}
+          </div>
+        </div>
+        <div className="container3 sec p-[20px]">
+          <div>
+            <h1 className="text-[64px] font-[800]">Affordable pricing</h1>
+            <p className="text-[#9f9e9e] font-[700]">
+              Bill me <span className="text-[#6C5CE7]"> monthly </span> • yearly
+            </p>
+          </div>
+          <div></div>
         </div>
       </div>
     );
--- src/Component/Card.jsx
+++ src/Component/Card.jsx
@@ -1,7 +1,23 @@
 import React from "react";
 
-const Card = () => {
-  return <div>Card</div>;
+const Card = ({ icon, title, des, bgColor }) => {
+  return (
+    <div className="flex flex-col justify-start items-start gap-3 max-md:max-w-[150px]">
+      <div
+        style={{ background: bgColor }}
+        className={`p-1 rounded-full w-[40px] h-[40px] flex items-center max-md:rounded-[15px]`}
+      >
+        <img src={icon} alt="" />
+      </div>
+      <div className="bolder max-md:text-[18px]">{title}</div>
+      <div className="text-[18px] font-[400] max-w-[270px] max-md:text-[13px] max-md:max-w-full">
+        {des}
+      </div>
+      <button className="p-[8px_18px] text-[#6C5CE7] rounded-[16px] border border-[#ADADAD]">
+        Learn more
+      </button>
+    </div>
+  );
 };
 
 export default Card;
